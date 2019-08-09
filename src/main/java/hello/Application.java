24186f4ea7fe890920f145965643ebb6df81bc66
sjcgithub <443433832@qq.com>
2019-08-09 13:53:44 +0800

    测试第一次提交。

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -20,6 +20,7 @@ public class Application {
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
+            System.out.println("test is success+，，，，，，，，，，，，，，，，，，，，，，，");
         }
     }
 }
